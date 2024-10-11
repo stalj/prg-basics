3164bdc94becc9d14bc508e67b3462c50957477f
Khrystynochka <krystyna.koshova@gmai.com>
2024-10-11 09:46:13 +0200

    fixed swap.py

--- 02-TypesAndVariables/swap.py
+++ 02-TypesAndVariables/swap.py
@@ -1,16 +1,13 @@
-def swap (a, b) :
-    z = 0
-    a = b
-    b = z
-    z = a
-
-
 num1 = 4
 num2 = 9
 
 print(f"before swap: a={num1} b={num2}")
 
-swap(num1, num2)
+num3 = 0
+
+num3 = num1
+num1 = num2
+num2 = num3
 
 print(f"after swap: a={num1} b={num2}")
 
