8578ef3340da8f53d67b9b34f322be57b8b21bfe
Khrystynochka <krystyna.koshova@gmai.com>
2024-10-11 10:08:33 +0200

    section 4 of unit 2

---
+++ 02-TypesAndVariables/data_print.py
@@ -0,0 +1,7 @@
+name = "Adam"
+age = 18
+height = 160
+
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {18 + 6} years old.")
---
+++ 02-TypesAndVariables/income_per_person.py
@@ -0,0 +1,6 @@
+father_income = 5450
+mother_income = 4920
+family_members = 5
+total_income = father_income + mother_income
+income_per_person = total_income/family_members
+print(f'Total family income is {total_income}, and income per person is {income_per_person}')
