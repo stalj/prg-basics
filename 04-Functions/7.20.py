edc9e2ee6a2a1da13b3352f1a7634508395fed32
Ignacy Dudziak <ignacydudziak@gmail.com>
2024-11-04 22:08:12 +0100

    fixed

--- 04-Functions/7.20.py
+++ 04-Functions/7.20.py
@@ -1,7 +1,7 @@
 def sum_repeated(number):
     suma=0
     number_str=str(number)
-    for i in range(9):
+    for i in range(10):
         count=number_str.count(str(i))
         if count>1:
             suma+=count*i    
