15ef768ef22c559b2ad3a23e50befb8d9897bcec
Khrystyna <khrystyna.koshova@gmail.com>
2024-11-12 08:30:28 +0100

    fix dice.py

--- 04-Functions/section 6/most_dice/dice.py
+++ 04-Functions/section 6/most_dice/dice.py
@@ -1,7 +1,5 @@
 def check_max(number):
-    string = str(number)
-    list_num = list(string)
     res = [0] * 10
-    for i in list_num:
-        res[int(i)] += 1
+    for digit in str(number):
+        res[int(digit)] += 1
     print(res.index(max(res)))
